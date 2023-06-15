841133b953e344ab22b0485c52b4f4b96a039ca5
baniiiiiiiiii <118009209+baniadamtampubolon@users.noreply.github.com>
2023-06-15 21:32:45 +0700

    Add files via upload

---
+++ level/tilesets/bgtree.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="bgtree" tilewidth="108" tileheight="312" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="108" height="312" source="../../graphics/terrain/Tree_bg/tree1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="88" height="144" source="../../graphics/terrain/Tree_bg/tree2.png"/>
+ </tile>
+</tileset>
---
+++ level/tilesets/crates.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="crates" tilewidth="58" tileheight="42" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="58" height="42" source="../../graphics/terrain/crate.png"/>
+ </tile>
+ <tile id="1">
+  <image width="30" height="27" source="../../graphics/terrain/crate2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="30" height="29" source="../../graphics/terrain/crate3.png"/>
+ </tile>
+</tileset>
